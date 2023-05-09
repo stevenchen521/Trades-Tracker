cf9c5db4550bfaeac42d25e27f748ad21d92f0ea
Steven CHEN <stevenchen521@gmail.com>
2023-05-09 09:23:59 +0800

    header1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import Sidebar from "./components/Sidebar.jsx";
+import Header from "./components/Header.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
 
 const App = () => {
-    return (
-        <div>
-            <h1 className="text-3xl font-bold underline">
-                Hello world!
-            </h1>
-        </div>
-    );
+  return (
+    <div className="flex h-full border-2">
+      <aside className="h-full border-2">
+        <Sidebar />
+      </aside>
+      <main className="flex flex-col">
+        <Header />
+        <Dashboard />
+      </main>
+    </div>
+  );
 };
 
 export default App;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,9 @@
-import React from 'react';
-
 const Sidebar = () => {
-    return (
-        <div>
-       Siderbar
-        </div>
-    );
+  return (
+    <nav className="flex flex-col text-white">
+      <h1>Trades Tracker</h1>
+    </nav>
+  );
 };
 
 export default Sidebar;
